1e1ad0375a1c1ec53e176e3c7d78cde69969217c
gianei <gianei.sebastiany@gmail.com>
2014-06-16 11:46:35 +0100

    Showing Video

--- WheelChairCollaborativeGame/WheelChairCollaborativeGame/Game1.cs
+++ WheelChairCollaborativeGame/WheelChairCollaborativeGame/Game1.cs
@@ -9,6 +9,8 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
+using Microsoft.Kinect;
+
 namespace WheelChairCollaborativeGame
 {
     /// <summary>
@@ -18,6 +20,13 @@ namespace WheelChairCollaborativeGame
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+
+        KinectSensor kinectSensor;
+
+        //TODO
+        string connectedStatus = "Not connected";
+        Texture2D kinectRGBVideo;
 
         public Game1()
         {
@@ -33,7 +42,8 @@ namespace WheelChairCollaborativeGame
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            KinectSensor.KinectSensors.StatusChanged += new EventHandler<StatusChangedEventArgs>(KinectSensors_StatusChanged);
+            DiscoverKinectSensor();
 
             base.Initialize();
         }
@@ -46,6 +56,8 @@ namespace WheelChairCollaborativeGame
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            kinectRGBVideo = new Texture2D(GraphicsDevice, 1337, 1337);
 
             // TODO: use this.Content to load your game content here
         }
@@ -83,9 +95,170 @@ namespace WheelChairCollaborativeGame
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(kinectRGBVideo, new Rectangle(0, 0, 640, 480), Color.White);
+            spriteBatch.End();
+
 
             base.Draw(gameTime);
+        }
+
+
+
+
+
+
+
+
+        void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            if (this.kinectSensor == e.Sensor)
+            {
+                if (e.Status == KinectStatus.Disconnected ||
+                    e.Status == KinectStatus.NotPowered)
+                {
+                    this.kinectSensor = null;
+                    this.DiscoverKinectSensor();
+                }
+            }
+        }
+
+        private bool InitializeKinect()
+        {
+            // Color stream
+            kinectSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            kinectSensor.ColorFrameReady += new EventHandler<ColorImageFrameReadyEventArgs>(kinectSensor_ColorFrameReady);
+
+            // Skeleton Stream
+            kinectSensor.SkeletonStream.Enable(new TransformSmoothParameters()
+            {
+                Smoothing = 0.5f,
+                Correction = 0.5f,
+                Prediction = 0.5f,
+                JitterRadius = 0.05f,
+                MaxDeviationRadius = 0.04f
+            });
+            kinectSensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(kinectSensor_SkeletonFrameReady);
+
+            try
+            {
+                kinectSensor.Start();
+            }
+            catch
+            {
+                //connectedStatus = "Unable to start the Kinect Sensor";
+                return false;
+            }
+            return true;
+        }
+
+        void kinectSensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    Skeleton[] skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
+
+                    skeletonFrame.CopySkeletonDataTo(skeletonData);
+                    Skeleton playerSkeleton = (from s in skeletonData where s.TrackingState == SkeletonTrackingState.Tracked select s).FirstOrDefault();
+                    if (playerSkeleton != null)
+                    {
+                        Joint head = playerSkeleton.Joints[JointType.Head];
+                        Console.Write("head position: " + head.Position.X + ", " + head.Position.Y + "\n");
+                        //headPositionPixels = new Vector2((((0.5f * head.Position.X) + 0.5f) * (640)), (((-0.5f * head.Position.Y) + 0.5f) * (480)));
+                    }
+
+
+
+                }
+
+            }
+        }
+
+        private void DiscoverKinectSensor()
+        {
+            foreach (KinectSensor sensor in KinectSensor.KinectSensors)
+            {
+                if (sensor.Status == KinectStatus.Connected)
+                {
+                    // Found one, set our sensor to this
+                    kinectSensor = sensor;
+                    break;
+                }
+            }
+
+            if (this.kinectSensor == null)
+            {
+                connectedStatus = "Found none Kinect Sensors connected to USB";
+                return;
+            }
+
+            // You can use the kinectSensor.Status to check for status
+            // and give the user some kind of feedback
+            switch (kinectSensor.Status)
+            {
+                case KinectStatus.Connected:
+                    {
+                        connectedStatus = "Status: Connected";
+                        break;
+                    }
+                case KinectStatus.Disconnected:
+                    {
+                        connectedStatus = "Status: Disconnected";
+                        break;
+                    }
+                case KinectStatus.NotPowered:
+                    {
+                        connectedStatus = "Status: Connect the power";
+                        break;
+                    }
+                default:
+                    {
+                        connectedStatus = "Status: Error";
+                        break;
+                    }
+            }
+
+            // Init the found and connected device
+            if (kinectSensor.Status == KinectStatus.Connected)
+            {
+                InitializeKinect();
+            }
+        }
+        public float Scale(float value, int max)
+        {
+            return MathHelper.Clamp((max >> 1) + (value * (max >> 1)), 0, max);
+        }
+        void kinectSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorImageFrame = e.OpenColorImageFrame())
+            {
+                if (colorImageFrame != null)
+                {
+
+                    byte[] pixelsFromFrame = new byte[colorImageFrame.PixelDataLength];
+
+                    colorImageFrame.CopyPixelDataTo(pixelsFromFrame);
+
+                    Color[] color = new Color[colorImageFrame.Height * colorImageFrame.Width];
+                    kinectRGBVideo = new Texture2D(graphics.GraphicsDevice, colorImageFrame.Width, colorImageFrame.Height);
+
+                    // Go through each pixel and set the bytes correctly
+                    // Remember, each pixel got a Rad, Green and Blue
+                    int index = 0;
+                    for (int y = 0; y < colorImageFrame.Height; y++)
+                    {
+                        for (int x = 0; x < colorImageFrame.Width; x++, index += 4)
+                        {
+                            color[y * colorImageFrame.Width + x] = new Color(pixelsFromFrame[index + 2], pixelsFromFrame[index + 1], pixelsFromFrame[index + 0]);
+                        }
+                    }
+
+                    // Set pixeldata from the ColorImageFrame to a Texture2D
+                    kinectRGBVideo.SetData(color);
+                }
+            }
         }
     }
 }
